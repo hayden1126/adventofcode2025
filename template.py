8d6e599dc96ef82159a08e07e881b65383c82d6e
hayden1126 <haydenleung1126@gmail.com>
2025-12-02 20:33:36 -0800

    fixed bug

--- template.py
+++ template.py
@@ -3,7 +3,7 @@
 import sys
 
 def main():
-    filename = "in.txt" if len(sys.argv) == 1 else sys.argv[0]
+    filename = "in.txt" if len(sys.argv) == 1 else sys.argv[1]
     with open(filename, 'r') as f:
         lines = [l for l in f.readlines() if l.strip()]
 

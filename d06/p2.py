97772689de376575049197c552aadc827d35bc89
hayden1126 <haydenleung1126@gmail.com>
2025-12-06 14:22:19 -0800

    done d6p2

---
+++ d06/p2.py
@@ -0,0 +1,52 @@
+#!/bin/python3
+
+import sys
+import math
+
+def calc(operands, start, stop, operator):
+    assert(stop >= start)
+    
+    rng = stop-start
+    vals = [0 for _ in range(rng)]
+    for l in operands:
+        for i in range(rng):
+            if l[start+i].isnumeric():
+                vals[i] = vals[i]*10 + int(l[start+i])
+    
+    if operator == '+':
+        return sum(vals)
+    if operator == '*':
+        return math.prod(vals)
+
+def main():
+    filename = "in.txt" if len(sys.argv) == 1 else sys.argv[1]
+    
+    lines = []
+    operators = []
+
+    with open(filename, 'r') as f:
+        lines = [l for l in f.readlines() if l.strip()]
+
+    operators = lines[-1]
+   
+    result = 0
+    start = 0
+    op = operators[start] # first operator
+    assert(op.strip() in ['*', '+'])
+    
+    # find next operator
+    for idx in range(start+1, len(operators)):
+        if operators[idx].isspace():
+            continue
+        result += calc(lines[:-1], start, idx-1, op)
+        
+        start = idx
+        op = operators[start]
+    
+    # reached the end, no next operator
+    result += calc(lines[:-1], start, len(operators), op)
+
+    print(result)
+
+main()
+
